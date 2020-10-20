2cdb6677e6951156a6a1fbee3fcefdf784b22ffa
Leonardo Cardoso <leocardosodev@gmail.com>
2020-10-19 22:27:07 -0300

    feat: add Provider in CustomDrawer

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -1,4 +1,6 @@
+import 'package:ecommerce_completo/models/page_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class DrawerTile extends StatelessWidget {
   final IconData iconData;
@@ -8,19 +10,26 @@ class DrawerTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final int currentPage = context.watch<PageManager>().page;
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        context.read<PageManager>().setPage(page);
+      },
       child: SizedBox(
         height: 60,
         child: Row(
           children: [
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 32),
-              child: Icon(iconData, size: 32, color: Colors.grey[700]),
+              child: Icon(iconData,
+                  size: 32,
+                  color: currentPage == page ? Colors.red : Colors.grey[700]),
             ),
             Text(
               title,
-              style: TextStyle(fontSize: 16, color: Colors.grey[700]),
+              style: TextStyle(
+                  fontSize: 16,
+                  color: currentPage == page ? Colors.red : Colors.grey[700]),
             ),
           ],
         ),
